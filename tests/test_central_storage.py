495b433e659e89783353b6aa141d4ff7572b1e73
Ludovic Danis <ludovic.danis@forvia.com>
2024-09-27 16:12:10 +0200

    Unit test for central storage

---
+++ tests/test_central_storage.py
@@ -0,0 +1,50 @@
+import simpy
+
+from utils import CentralStorage
+
+# Class to create the simpy Environment and set up the central storage. 
+class Manuf:
+    def __init__(self) -> None:
+        self.env = simpy.Environment()
+        self.config = {
+    'front': [
+        {'allowed_sizes' : ['ref A', 'ref B'],
+         'capacity': 336},
+        {'allowed_sizes' : ['ref A', 'ref B', 'ref C'],
+         'capacity': 3}],
+
+    'back': [
+        {'allowed_sizes' : ['ref A', 'ref B'],
+         'capacity': 376}]
+    }
+        self.central_storage = CentralStorage(self.env, self.config)
+
+# Tests are below. Run the file and look at the terminal (details are printed for each step).
+m = Manuf()
+print(m.central_storage)
+m.central_storage.available_spot('ref A')
+m.central_storage.available_spot('ref C')
+m.central_storage.available_spot('ref D')
+
+m.central_storage.put('ref C')
+print(m.central_storage)
+
+m.central_storage.available_spot('ref C')
+m.central_storage.put('ref C')
+print(m.central_storage)
+
+m.central_storage.get('ref C')
+print(m.central_storage)
+
+print("Add 337 'ref A' to fill the first block and add 1 ref A to the next block.")
+for _ in range(337):
+    m.central_storage.put('ref A')
+print(m.central_storage)
+
+m.central_storage.available_spot('ref C')
+m.central_storage.put('ref C')
+print(m.central_storage)
+
+m.central_storage.available_spot('ref C')
+m.central_storage.put('ref C')
+print(m.central_storage)
